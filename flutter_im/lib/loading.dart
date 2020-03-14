4c2a92d4c962aff31c1a7edfbd99b4b993de6657
贾大伟 <jiadawei80@126.com>
2020-03-14 14:49:32 +0800

    1. 添加聊天室 demo。2. 启动加载页完成。

---
+++ flutter_im/lib/loading.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class LoadingPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _LoadingState();
+  }
+}
+
+class _LoadingState extends State<LoadingPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), (){
+      print("Future im app is displaying...");
+//      Navigator.of(context).pushReplacementNamed("app")
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Stack(
+        children: <Widget>[
+          Image.asset('images/loading.jpeg', fit: BoxFit.cover,),
+        ],
+      ),
+    );
+  }
+}
